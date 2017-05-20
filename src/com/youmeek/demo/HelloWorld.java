8016fb627ba8041a6966f2a69acdce48f44e7123
a516117636 <516117636@qq.com>
2017-05-21 01:10:29 +0800

    Initial commit

---
+++ src/com/youmeek/demo/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.youmeek.demo;
+
+/**
+ * Created by 51611 on 2017/5/21.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
